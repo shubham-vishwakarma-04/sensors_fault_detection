13b93c479e9f7fa280f7dcea40c9a46d7de003b5
shubham-vishwakarma-004 <sv.shubham004@gmail.com>
2025-04-17 00:15:06 +0530

    Completed all modules

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -7,7 +7,7 @@ MONGO_COLLECTION_NAME = "waferfault"
 
 
 TARGET_COLUMN = "quality"
-MONGO_DB_URL="mongodb+srv://imran:TdPLW9Ad0OzpSSD2@cluster0.fv0lm61.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_DB_URL="mongodb+srv://shubham:12345@cluster0.kthp7kz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 
 MODEL_FILE_NAME = "model"
